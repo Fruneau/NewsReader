7c7f1bc951bd9293d377157d521e58a659df64fa
Florent Bruneau <florent.bruneau@intersec.com>
2015-07-14 12:20:25 +0200

    Initial Commit

---
+++ NewsReader/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NewsReader
+//
+//  Created by Florent Bruneau on 14/07/2015.
+//  Copyright © 2015 Florent Bruneau. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
