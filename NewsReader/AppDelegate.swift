501a90e23f03256e87142b20a68c03bd5a678710
Florent Bruneau <florent.bruneau@intersec.com>
2015-08-05 09:18:12 +0200

    Create a dedicate object to control the thread view.

--- NewsReader/AppDelegate.swift
+++ NewsReader/AppDelegate.swift
@@ -446,68 +446,23 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-    @IBOutlet weak var articleViewController: ArticleViewController!
+    @IBOutlet weak var threadViewController: ThreadViewController!
+
     /* Groups view */
     @IBOutlet weak var groupTreeController: NSTreeController!
     dynamic var groupRoots = [GroupTree(root: "Groups")]
     var groupIndexes : [NSIndexPath] = [] {
         didSet {
             if self.groupIndexes.count == 0 {
-                self.currentGroup = nil
+                self.threadViewController.currentGroup = nil
             } else {
-                self.currentGroup = self.groupTreeController.selectedObjects[0] as? GroupTree
+                self.threadViewController.currentGroup = self.groupTreeController.selectedObjects[0] as? GroupTree
             }
         }
     }
-    var currentGroup : GroupTree? {
-        didSet {
-            if oldValue === self.currentGroup {
-                return
-            }
-
-            oldValue?.delegate = nil
-            var oldPaths = Set<NSIndexPath>()
-            if let roots = oldValue?.roots {
-                for i in 0..<roots.count {
-                    oldPaths.insert(NSIndexPath(forItem: i, inSection: 0))
-                }
-            }
-
-            var newPaths = Set<NSIndexPath>()
-            if let roots = self.currentGroup?.roots {
-                for i in 0..<roots.count {
-                    newPaths.insert(NSIndexPath(forItem: i, inSection: 0))
-                }
-            }
-            self.currentGroup?.delegate = self
-            self.currentGroup?.load()
-
-            if newPaths.count == 0 && oldPaths.count == 0 {
-                return
-            }
-
-            self.threadView.performBatchUpdates({
-                if oldPaths.count > 0 {
-                    self.threadView.deleteItemsAtIndexPaths(oldPaths)
-                }
-                if newPaths.count > 0 {
-                    self.threadView.insertItemsAtIndexPaths(newPaths)
-                }
-            }, completionHandler: { (_) in () })
-        }
-    }
-
-    /* Thread view */
-    @IBOutlet weak var threadView: NSCollectionView!
 
     /* Model handling */
     var nntp : NNTPClient?
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        self.threadView.minItemSize = NSMakeSize(0, 37)
-        self.threadView.maxItemSize = NSMakeSize(0, 37)
-    }
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         guard var rcContent = NSData(contentsOfFile: "~/.newsreaderrc".stringByStandardizingPath)?.utf8String else {
@@ -550,99 +505,5 @@ extension AppDelegate : NSOutlineViewDelegate {
         let node = item.representedObject as! GroupTree
 
         return outlineView.makeViewWithIdentifier(node.isRoot ? "HeaderCell" : "DataCell", owner: self)
-    }
-}
-
-class ThreadViewItem : SelectableCollectionViewItem {
-    @IBOutlet weak var fromView: NSTextField!
-    @IBOutlet weak var dateView: NSTextField!
-    @IBOutlet weak var subjectView: NSTextField!
-    @IBOutlet weak var threadCountView: NSTextField!
-
-    override var representedObject : AnyObject? {
-        didSet {
-            let article = self.representedObject as? Article
-
-            self.fromView.objectValue = article?.from
-            self.dateView.objectValue = article?.date
-            self.subjectView.objectValue = article?.subject
-            self.threadCountView.objectValue = article?.threadCount
-        }
-    }
-}
-
-extension AppDelegate : NSCollectionViewDataSource, NSCollectionViewDelegate {
-    private func threadForIndexPath(indexPath: NSIndexPath) -> Article? {
-        guard indexPath.section == 0 else {
-            return nil
-        }
-
-        guard let roots = self.currentGroup?.roots else {
-            return nil
-        }
-
-        return roots[indexPath.item]
-    }
-
-    private func indexPathForThread(article: Article) -> NSIndexPath? {
-        guard let idx = self.currentGroup?.roots?.indexOf(article) else {
-            return nil
-        }
-
-        return NSIndexPath(forItem: idx, inSection: 0)
-    }
-
-    func collectionView(collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
-        guard let roots = self.currentGroup?.roots else {
-            return 0
-        }
-
-        return roots.count
-    }
-
-    func collectionView(collectionView: NSCollectionView, itemForRepresentedObjectAtIndexPath indexPath: NSIndexPath) -> NSCollectionViewItem {
-        let item = collectionView.makeItemWithIdentifier("Thread", forIndexPath: indexPath)
-
-        item.representedObject = self.threadForIndexPath(indexPath)
-        return item
-    }
-
-    func collectionView(collectionView: NSCollectionView, layout collectionViewLayout: NSCollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> NSSize {
-        let size = collectionView.frame.size
-
-        return NSSize(width: size.width, height: 37)
-    }
-
-    private func updateCurrentThread() {
-        let selected = self.threadView.selectionIndexPaths
-
-        if selected.count == 0 {
-            self.articleViewController.currentThread = nil
-        } else {
-            self.articleViewController.currentThread = self.threadForIndexPath(selected.first!)
-        }
-    }
-
-    func collectionView(collectionView: NSCollectionView, didSelectItemsAtIndexPaths indexPaths: Set<NSIndexPath>) {
-        self.updateCurrentThread()
-    }
-
-    func collectionView(collectionView: NSCollectionView, didDeselectItemsAtIndexPaths indexPaths: Set<NSIndexPath>) {
-        self.updateCurrentThread()
-    }
-}
-
-extension AppDelegate : GroupTreeDelegate {
-    func groupTree(groupTree: GroupTree, hasNewThreads articles: [Article]) {
-        var indexPaths = Set<NSIndexPath>()
-        for article in articles {
-            if let indexPath = self.indexPathForThread(article) {
-                indexPaths.insert(indexPath)
-            }
-        }
-
-        if indexPaths.count > 0 {
-            self.threadView.insertItemsAtIndexPaths(indexPaths)
-        }
     }
 }
---
+++ NewsReader/ThreadView.swift
@@ -0,0 +1,144 @@
+//
+//  ThreadView.swift
+//  NewsReader
+//
+//  Created by Florent Bruneau on 05/08/2015.
+//  Copyright © 2015 Florent Bruneau. All rights reserved.
+//
+
+import Cocoa
+
+class ThreadViewItem : SelectableCollectionViewItem {
+    @IBOutlet weak var fromView: NSTextField!
+    @IBOutlet weak var dateView: NSTextField!
+    @IBOutlet weak var subjectView: NSTextField!
+    @IBOutlet weak var threadCountView: NSTextField!
+
+    override var representedObject : AnyObject? {
+        didSet {
+            let article = self.representedObject as? Article
+
+            self.fromView.objectValue = article?.from
+            self.dateView.objectValue = article?.date
+            self.subjectView.objectValue = article?.subject
+            self.threadCountView.objectValue = article?.threadCount
+        }
+    }
+}
+
+class ThreadViewController : NSObject, NSCollectionViewDataSource, NSCollectionViewDelegate {
+    @IBOutlet weak var threadView: NSCollectionView!
+    @IBOutlet weak var articleViewController: ArticleViewController!
+
+    var currentGroup : GroupTree? {
+        didSet {
+            if oldValue === self.currentGroup {
+                return
+            }
+
+            oldValue?.delegate = nil
+            var oldPaths = Set<NSIndexPath>()
+            if let roots = oldValue?.roots {
+                for i in 0..<roots.count {
+                    oldPaths.insert(NSIndexPath(forItem: i, inSection: 0))
+                }
+            }
+
+            var newPaths = Set<NSIndexPath>()
+            if let roots = self.currentGroup?.roots {
+                for i in 0..<roots.count {
+                    newPaths.insert(NSIndexPath(forItem: i, inSection: 0))
+                }
+            }
+            self.currentGroup?.delegate = self
+            self.currentGroup?.load()
+
+            if newPaths.count == 0 && oldPaths.count == 0 {
+                return
+            }
+
+            self.threadView.performBatchUpdates({
+                if oldPaths.count > 0 {
+                    self.threadView.deleteItemsAtIndexPaths(oldPaths)
+                }
+                if newPaths.count > 0 {
+                    self.threadView.insertItemsAtIndexPaths(newPaths)
+                }
+                }, completionHandler: { (_) in () })
+        }
+    }
+
+    private func threadForIndexPath(indexPath: NSIndexPath) -> Article? {
+        guard indexPath.section == 0 else {
+            return nil
+        }
+
+        guard let roots = self.currentGroup?.roots else {
+            return nil
+        }
+
+        return roots[indexPath.item]
+    }
+
+    private func indexPathForThread(article: Article) -> NSIndexPath? {
+        guard let idx = self.currentGroup?.roots?.indexOf(article) else {
+            return nil
+        }
+
+        return NSIndexPath(forItem: idx, inSection: 0)
+    }
+
+    func collectionView(collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
+        guard let roots = self.currentGroup?.roots else {
+            return 0
+        }
+
+        return roots.count
+    }
+
+    func collectionView(collectionView: NSCollectionView, itemForRepresentedObjectAtIndexPath indexPath: NSIndexPath) -> NSCollectionViewItem {
+        let item = collectionView.makeItemWithIdentifier("Thread", forIndexPath: indexPath)
+
+        item.representedObject = self.threadForIndexPath(indexPath)
+        return item
+    }
+
+    func collectionView(collectionView: NSCollectionView, layout collectionViewLayout: NSCollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> NSSize {
+        let size = collectionView.frame.size
+
+        return NSSize(width: size.width, height: 37)
+    }
+
+    private func updateCurrentThread() {
+        let selected = self.threadView.selectionIndexPaths
+
+        if selected.count == 0 {
+            self.articleViewController.currentThread = nil
+        } else {
+            self.articleViewController.currentThread = self.threadForIndexPath(selected.first!)
+        }
+    }
+
+    func collectionView(collectionView: NSCollectionView, didSelectItemsAtIndexPaths indexPaths: Set<NSIndexPath>) {
+        self.updateCurrentThread()
+    }
+
+    func collectionView(collectionView: NSCollectionView, didDeselectItemsAtIndexPaths indexPaths: Set<NSIndexPath>) {
+        self.updateCurrentThread()
+    }
+}
+
+extension ThreadViewController : GroupTreeDelegate {
+    func groupTree(groupTree: GroupTree, hasNewThreads articles: [Article]) {
+        var indexPaths = Set<NSIndexPath>()
+        for article in articles {
+            if let indexPath = self.indexPathForThread(article) {
+                indexPaths.insert(indexPath)
+            }
+        }
+
+        if indexPaths.count > 0 {
+            self.threadView.insertItemsAtIndexPaths(indexPaths)
+        }
+    }
+}
